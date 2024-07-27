8726a8d976a925d65bd9c86ceb32573659a3c412
Zaim Irfansyah <zaimirfansyah@gmail.com>
2024-07-27 18:47:15 +0700

    Commit awal

---
+++ server.js
@@ -0,0 +1,47 @@
+const Hapi = require('@hapi/hapi');
+const notes = require('./notes');
+const NotesService = require('./service/NotesService');
+const NotesValidator = require('./validator/notes');
+
+const init = async () => {
+  const notesService = new NotesService();
+  const server = Hapi.server({
+    port: 3000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+ 
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+      validator: NotesValidator
+    },
+  });
+
+  server.ext('onPreResponse', (request, h) => {
+    // mendapatkan konteks response dari request
+    const { response } = request;
+  
+    // penanganan client error secara internal.
+    if (response instanceof ClientError) {
+      const newResponse = h.response({
+        status: 'fail',
+        message: response.message,
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+      
+    return h.continue;
+  });
+ 
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+ 
+init();
