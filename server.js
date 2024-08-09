46f60359afe0001838637fe9166774ceb1da7997
Zaim Irfansyah <zaimirfansyah@gmail.com>
2024-08-09 22:50:52 +0700

    auth implemented

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
-const notes = require('./notes');
+const Jwt = require('@hapi/jwt');
+
+const notes = require('./api/notes');
 const NotesService = require('./service/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
+
 const ClientError = require('./exceptions/ClientError')
 
+const users = require('./api/users');
+const UsersService = require('./service/postgres/UserService');
+const UsersValidator = require('./validator/users');
+
+// authentications
+const authentications = require('./api/authentications');
+const AuthenticationsService = require('./service/postgres/AuthenticationsService');
+const TokenManager = require('./tokenize/TokenManager');
+const AuthenticationsValidator = require('./validator/authentications');
+
 const init = async () => {
+
   const notesService = new NotesService();
+  const usersService = new UsersService();
+  const authenticationsService = new AuthenticationsService();
+
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -17,14 +34,56 @@ const init = async () => {
       },
     },
   });
- 
-  await server.register({
-    plugin: notes,
-    options: {
-      service: notesService,
-      validator: NotesValidator
+
+  // registrasi plugin eksternal
+  await server.register([
+    {
+      plugin: Jwt,
     },
+  ]);
+
+  // mendefinisikan strategy autentikasi jwt
+  server.auth.strategy('notesapp_jwt', 'jwt', {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
   });
+ 
+  await server.register([
+    {
+      plugin: notes,
+      options: {
+        service: notesService,
+        validator: NotesValidator,
+      },
+    },
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+    {
+      plugin: authentications,
+      options: {
+        authenticationsService,
+        usersService,
+        tokenManager: TokenManager,
+        validator: AuthenticationsValidator,
+      },
+    },
+  ]);
 
   server.ext('onPreResponse', (request, h) => {
     // mendapatkan konteks response dari request
